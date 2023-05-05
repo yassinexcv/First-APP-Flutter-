1e98283007a53cd548f847df3336fe28c45974f0
yassinexcv <yassinexcv188@gmail.com>
2023-05-05 11:40:13 +0100

    Expanded(

--- lib/main.dart
+++ lib/main.dart
@@ -18,33 +18,37 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body:
-       Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Text('Hello World'),
-              Text('  Hello World'),
-            ],
+      Row(
+        children: [
+          Expanded(child: Image.asset('assets/images.jpg'),
+          flex: 3,
           ),
-          Container(
-            padding: EdgeInsets.all(20.0),
-            color: Colors.cyan,
-            child: Text('one'),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.cyan,
+              child: Text('1'),
+            ),
           ),
-          Container(
-            padding: EdgeInsets.all(30.0),
-            color: Colors.pinkAccent,
-            child: Text('two'),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.pinkAccent,
+              child: Text('2'),
+            ),
           ),
-          Container(
-            padding: EdgeInsets.all(40.0),
-            color: Colors.amber,
-            child: Text('three'),
-          ),
-        ],
-      ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.amber,
+              child: Text('3'),
+            ),
+          ),],
+      )
+      ,
        
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
