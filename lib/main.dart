6696a64a2227a76c2529b26b473d08f9b5774550
yassinexcv <yassinexcv188@gmail.com>
2023-05-05 11:55:33 +0200

    Containers & Padding

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,11 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body:
-       Center(
-        /* child: Text(
+      Container(
+
+        color: Colors.grey[400],
+        padding: EdgeInsets.all(20.0),
+        child: Text(
           'Hello World',
           style: TextStyle(
             fontSize: 20.0,
@@ -28,32 +31,9 @@ class Home extends StatelessWidget {
             color: Colors.grey[600],
             fontFamily: 'IndieFlower',
           ),
-        ), */
-        // child: Image.network('https://media.licdn.com/dms/image/C4E0BAQGlUAfHhcwifg/company-logo_200_200/0/1676284773816?e=2147483647&v=beta&t=XXBzNO3ts9k64MxI5r0j9Yz9OxFpFn7bSGEuRgRQ_r0'),
-
-       /*  child: Icon(
-          Icons.add_a_photo,
-          color: Colors.red[600],
-          size: 50.0,
-
-        ) */
-
-         child: ElevatedButton.icon(
-           onPressed: () {
-             print('You clicked me');
-           },
-           icon: Icon(
-             Icons.mail
-           ),
-           label: Text('Mail Me'),
-           style: ElevatedButton.styleFrom(
-             primary: Colors.amber,
-             onPrimary: Colors.white,
-           ),
-         )
-
-
-      ),
+        ),
+      )
+       ,
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text('Click Mee !'),
